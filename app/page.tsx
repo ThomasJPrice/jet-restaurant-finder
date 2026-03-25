3d714657b50c042148d7c11d280d00816cbb59e2
Thomas Price <thomasjprice2@gmail.com>
2026-03-25 11:18:50 +0000

    feat: restaurant searching from search params postcode

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
+import RestaurantCard from "@/components/RestaurantCard";
+import { fetchRestaurants } from "@/lib/restaurants";
+import { Restaurant } from "@/types/restaurant";
 
-export default function Home() {
+type HomeProps = {
+  searchParams: Promise<{
+    postcode?: string;
+  }>
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const params = await searchParams
+  const postcode = params.postcode?.trim()
+
+  let restaurants: Restaurant[] = [];
+
+  if (postcode) {
+    restaurants = await fetchRestaurants(postcode)
+  }
+
   return (
-    <div>
-      <main>
-        Main Page
-      </main>
-    </div>
+    <main className="max-w-3xl mx-auto px-6 py-8">
+
+      
+      {restaurants?.length > 0 ? (
+        <div className="grid grid-cols-1 gap-3">
+        {restaurants && restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+        ))}
+      </div>
+      ) : (
+        <div className="text-center">
+          <p>Enter your postcode to see near locations!</p>
+        </div>
+      )}
+    </main>
   );
 }
